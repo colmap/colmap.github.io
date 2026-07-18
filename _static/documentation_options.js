192eac2abb5260e514911f34571b9f61bb26ac59
ahojnnes <ahojnnes@users.noreply.github.com>
2026-07-18 07:48:41 +0000

    Update docs from colmap@66273ef365a09b0cf8b36ad9c072d6fb7407f57a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '4.2.0.dev0 | bd7c30b (2026-07-17)',
+    VERSION: '4.2.0.dev0 | 66273ef (2026-07-18)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
