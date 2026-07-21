d83aa62583277915b11bdea1c5fe407a77ec0c83
sarlinpe <sarlinpe@users.noreply.github.com>
2026-07-21 14:04:03 +0000

    Update docs from colmap@109ebdb5c5c3e330d90321d77ef4bbe55bb408d2

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '4.2.0.dev0 | dd7b269 (2026-07-21)',
+    VERSION: '4.2.0.dev0 | 109ebdb (2026-07-21)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
